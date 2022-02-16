80db6a4579a783a008fbc904fc808d8c408e06cb
Moddaveel <newsomedc@yahoo.com>
2022-02-15 20:50:32 -0600

    2-14-2022

--- Python/flask/fundamentals/counter/server.py
+++ Python/flask/fundamentals/counter/server.py
@@ -1,4 +1,19 @@
-if 'cnt' in session:
-        session['cnt']+=1
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'root'
+# our index route will handle rendering our form
+@app.route('/')
+def index():
+    if 'cnt' in session:
+        session['cnt'] += 1
     else:
-        session['cnt']=1
+        session['cnt'] = 1
+    return render_template("index.html")
+
+@
+
+@app.route('/counter', methods=['POST'])
+def create_user():
+
+    return redirect("/")
